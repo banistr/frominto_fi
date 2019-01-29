bdeacab73a2b07f7dfb0bc3f07a66c482adedb1f
petja <hello@petja.me>
2019-01-29 12:03:31 +0200

    Formatting code with Prettier

--- index.js
+++ index.js
@@ -3,151 +3,156 @@
  where the MOST SIGNIFICANT MATCH is the latest.
 */
 const ruleset = [
-    // extremely stupid initial vowels
-    [['a'], 0 , 'sta', 'ssa' , 'an'],
-    [['e'], 0 , 'stä', 'ssä' , 'en'],
-    [['i'], 0 , 'stä', 'ssä' , 'in'],
-    [['o'], 0 , 'sta', 'ssa' , 'on'],
-    [['u'], 0 , 'sta', 'ssa' , 'un'],
-    [['y'], 0 , 'stä', 'ssä' , 'yn'],
-    [['ä'], 0 , 'stä', 'ssä' , 'än'],
-    [['ö'], 0 , 'stä', 'ssä' , 'ön'],
-    
-    //-ri -erityiset: pori, meri etc. + kajaani yms
-    [['pori', 'uri', 'ari', 'ani'], 0, 'sta', 'ssa', 'in'],
-    [['meri','veri'], -1, 'essä', 'estä', 'ereen'],
+  // extremely stupid initial vowels
+  [['a'], 0, 'sta', 'ssa', 'an'],
+  [['e'], 0, 'stä', 'ssä', 'en'],
+  [['i'], 0, 'stä', 'ssä', 'in'],
+  [['o'], 0, 'sta', 'ssa', 'on'],
+  [['u'], 0, 'sta', 'ssa', 'un'],
+  [['y'], 0, 'stä', 'ssä', 'yn'],
+  [['ä'], 0, 'stä', 'ssä', 'än'],
+  [['ö'], 0, 'stä', 'ssä', 'ön'],
 
-    // s -päätteiset (Kannus, muhos, varkaus, karkaus)
-    [['s'], -1, 'ksesta', 'ksessa', 'kseen'],
-    [['Muhos'], -1, 'kselta', 'ksella', 'kselle'],
+  // -ri -erityiset: pori, meri etc. + kajaani yms
+  [['pori', 'uri', 'ari', 'ani'], 0, 'sta', 'ssa', 'in'],
+  [['meri', 'veri'], -1, 'essä', 'estä', 'ereen'],
 
-    [['aus'], -1, 'desta', 'dessa', 'teen'],
+  // s -päätteiset (Kannus, muhos, varkaus, karkaus)
+  [['s'], -1, 'ksesta', 'ksessa', 'kseen'],
+  [['Muhos'], -1, 'kselta', 'ksella', 'kselle'],
 
+  [['aus'], -1, 'desta', 'dessa', 'teen'],
 
-    [['as', 'es'], -1, 'ksesta', 'ksessa', 'kseen'],
-    //xxby, like Degerby
-    [['by'], 0 , 'stä', 'ssä' , 'hyn'],
+  [['as', 'es'], -1, 'ksesta', 'ksessa', 'kseen'],
 
-    //naantali et al
-    [['ali', 'oli', 'uli', 'nti', 'nni'], 0, 'sta', 'ssa', 'in'],
-    
-    //-valta etc.
-    [['lta'], -2, 'lasta', 'lassa', 'taan'],
-    //-he, like Raahe
-    [['he'], 0, 'sta', 'ssa', 'en'],
-    // -kka
-    [['kka'], -2, 'asta', 'assa', 'kaan'],
-    [['kkä'], -2, 'ästä', 'ässä', 'kään'],
-    //-nta, esim. maalaiskunta, lappeenranta
-    [['nta'], -2 , 'nasta', 'nassa' , 'taan'],
+  // xxby, like Degerby
+  [['by'], 0, 'stä', 'ssä', 'hyn'],
 
-    // helsinki, hanko, nta, nki etc.
-    [['nki'], -3 , 'ngistä', 'ngissä' , 'nkiin'],
-    [['nko'], -3 , 'ngosta', 'ngossa' , 'nkoon'],
+  // naantali et al
+  [['ali', 'oli', 'uli', 'nti', 'nni'], 0, 'sta', 'ssa', 'in'],
 
-    //tampere + kempele etc
-    [['pere'], 0 , 'elta', 'ella' , 'elle'],
-    [['pele'], 0 , 'eltä', 'ellä' , 'elle'],
+  // -valta etc.
+  [['lta'], -2, 'lasta', 'lassa', 'taan'],
 
-    [['rku', 'kku'], -2 , 'usta', 'ussa' , 'kuun'],
-    //ii, aa - päätteiset, kuten Ii, laukaa, vantaa, espoo
-    [['ee'], 0, 'ltä', 'llä', 'lle'],
-    [['ii'], 0, 'stä', 'ssä', 'hin'],
-    [['uu'], 0, 'sta', 'ssa', 'hun'],
-    [['aa', 'oo'], 0, 'sta', 'ssa', 'seen'],
-    [['maa', 'taa', 'ria', 'pua', 'uma', 'tra', 'ava', 'jaa', 'ruu', 'kia'], 0, 'lta', 'lla', 'lle'],
+  // -he, like Raahe
+  [['he'], 0, 'sta', 'ssa', 'en'],
 
-    [['iä', 'kää', 'pää'], 0, 'ltä', 'llä', 'lle'],
+  // -kka
+  [['kka'], -2, 'asta', 'assa', 'kaan'],
+  [['kkä'], -2, 'ästä', 'ässä', 'kään'],
 
-    [['vik'], 0, 'istä', 'issä', 'iin'],
-    //mm. savio
-    [['vio'], 0, 'lta', 'lla', 'lle'],
-    
-    //inkeroinen, kauniainen, änkeröinen, kimpeläinen yms.
-    [['inen'], -4, 'isista', 'isissa', 'isiin' ],
-    [['äinen', 'öinen'], -4, 'isistä', 'isissä', 'isiin' ],
-    [['Joroinen'], -4, 'isilta', 'isilla', 'isille' ],
+  // -nta, esim. maalaiskunta, lappeenranta
+  [['nta'], -2, 'nasta', 'nassa', 'taan'],
 
-    //harjut + erikoisharjut
-    [['harju'], 0, 'lta', 'lla', 'lle' ],
-    [['hiekkaharju', 'uimaharju'], 0, 'sta', 'ssa', 'un' ],
+  // helsinki, hanko, nta, nki etc.
+  [['nki'], -3, 'ngistä', 'ngissä', 'nkiin'],
+  [['nko'], -3, 'ngosta', 'ngossa', 'nkoon'],
 
-    [['nummi'], -1, 'elta', 'ella', 'elle' ],
-    // järvet ja vedet
-    [['järvi'], -1, 'eltä', 'ellä', 'elle' ],
-    [['vesi'], -2, 'deltä', 'dellä', 'delle' ],
-    // genetiivinpää
-    [['npää'], 0, 'stä', 'ssä', 'hän'],
-    //kosket ja salmet
-    [['koski', 'salmi'], -1, 'elta', 'ella', 'elle' ],
-    [['ore'], 0, 'elta', 'ella', 'elle' ],
+  // tampere + kempele etc
+  [['pere'], 0, 'elta', 'ella', 'elle'],
+  [['pele'], 0, 'eltä', 'ellä', 'elle'],
 
-    //saaret + erikoissaaret
-    [['saari'], -1, 'esta', 'essa', 'een' ],
-    
-    //lahdet + erikoislahdet
-    [['lahti'], -2, 'desta', 'dessa', 'teen'],
-    [['kesälahti', 'talvilahti','lapinlahti'], -2, 'delta', 'della', 'delle'],
+  [['rku', 'kku'], -2, 'usta', 'ussa', 'kuun'],
 
-    //vuoret + erikoisvuoret
-    [['vuori'], -1, 'elta', 'ella', 'elle'],
-    [['laajavuori', 'punavuori'], -1, 'esta', 'essa', 'een'],
-    //joet
-    [['joki'], -4, 'joelta', 'joella', 'joelle' ],
-    
-    //mäet
-    [['mäki'], -4, 'mäeltä', 'mäellä', 'mäelle' ],
-    [['tunturi'], 0, 'lta', 'lla', 'lle' ],
-    //erikoismäet
-    [['kannelmäki', 'pukinmäki', 'myyrmäki'], -2, 'estä', 'essä', 'keen' ],
-    
-    //asemat, hotellit ja muut
-    [['asema'], 0, 'lta', 'lla', 'lle'],  
-    [['otelli'], 0, 'sta', 'ssa', 'in'],  
-    [['hovi'], 0, 'sta', 'ssa', 'in'],  
+  // ii, aa - päätteiset, kuten Ii, laukaa, vantaa, espoo
+  [['ee'], 0, 'ltä', 'llä', 'lle'],
+  [['ii'], 0, 'stä', 'ssä', 'hin'],
+  [['uu'], 0, 'sta', 'ssa', 'hun'],
+  [['aa', 'oo'], 0, 'sta', 'ssa', 'seen'],
+  [['maa', 'taa', 'ria', 'pua', 'uma', 'tra', 'ava', 'jaa', 'ruu', 'kia'], 0, 'lta', 'lla', 'lle'],
 
-    //-selkä
-    [['selkä'], -2, 'ältä', 'ällä', 'älle'],  
-    
-    //-etti, tough one (taavetti, retretti)
-    [['tti'], -2, 'ista', 'issa', 'tiin'],  
-    [['kki'], -2, 'ista', 'issa', 'kiin'],  
-    [['retti', 'pirtti'], -2, 'istä', 'issä', 'tiin'],  
+  [['iä', 'kää', 'pää'], 0, 'ltä', 'llä', 'lle'],
 
-    //tuntureiden nimet
-    [['Suomu', 'Ruka', 'Tahko', 'Luosto', 'Malmi'], 0, 'lta', 'lla', 'lle'],
-    [['Levi', 'Pyhä'], 0, 'ltä', 'llä', 'lle'],
+  [['vik'], 0, 'istä', 'issä', 'iin'],
 
-    // totally weird cases & one-off hacks
-    [['Kangasala'], -3, 'alta', 'alla', 'alle'],
-    // Helsinki Lentoasema, Turenki Lentoasema (if such exists)
-    [['nki Lentoasema'], -14, 'ngin Lentoasemalta', 'ngin Lentoasemalla', 'ngin Lentoasemalle'],
-    
+  // mm. savio
+  [['vio'], 0, 'lta', 'lla', 'lle'],
 
-];
+  // inkeroinen, kauniainen, änkeröinen, kimpeläinen yms.
+  [['inen'], -4, 'isista', 'isissa', 'isiin'],
+  [['äinen', 'öinen'], -4, 'isistä', 'isissä', 'isiin'],
+  [['Joroinen'], -4, 'isilta', 'isilla', 'isille'],
+
+  // harjut + erikoisharjut
+  [['harju'], 0, 'lta', 'lla', 'lle'],
+  [['hiekkaharju', 'uimaharju'], 0, 'sta', 'ssa', 'un'],
+
+  [['nummi'], -1, 'elta', 'ella', 'elle'],
+  // järvet ja vedet
+  [['järvi'], -1, 'eltä', 'ellä', 'elle'],
+  [['vesi'], -2, 'deltä', 'dellä', 'delle'],
+  // genetiivinpää
+  [['npää'], 0, 'stä', 'ssä', 'hän'],
+  // kosket ja salmet
+  [['koski', 'salmi'], -1, 'elta', 'ella', 'elle'],
+  [['ore'], 0, 'elta', 'ella', 'elle'],
+
+  // saaret + erikoissaaret
+  [['saari'], -1, 'esta', 'essa', 'een'],
+
+  // lahdet + erikoislahdet
+  [['lahti'], -2, 'desta', 'dessa', 'teen'],
+  [['kesälahti', 'talvilahti', 'lapinlahti'], -2, 'delta', 'della', 'delle'],
+
+  // vuoret + erikoisvuoret
+  [['vuori'], -1, 'elta', 'ella', 'elle'],
+  [['laajavuori', 'punavuori'], -1, 'esta', 'essa', 'een'],
+  //joet
+  [['joki'], -4, 'joelta', 'joella', 'joelle'],
+
+  // mäet
+  [['mäki'], -4, 'mäeltä', 'mäellä', 'mäelle'],
+  [['tunturi'], 0, 'lta', 'lla', 'lle'],
+  // erikoismäet
+  [['kannelmäki', 'pukinmäki', 'myyrmäki'], -2, 'estä', 'essä', 'keen'],
+
+  // asemat, hotellit ja muut
+  [['asema'], 0, 'lta', 'lla', 'lle'],
+  [['otelli'], 0, 'sta', 'ssa', 'in'],
+  [['hovi'], 0, 'sta', 'ssa', 'in'],
+
+  // -selkä
+  [['selkä'], -2, 'ältä', 'ällä', 'älle'],
+
+  // -etti, tough one (taavetti, retretti)
+  [['tti'], -2, 'ista', 'issa', 'tiin'],
+  [['kki'], -2, 'ista', 'issa', 'kiin'],
+  [['retti', 'pirtti'], -2, 'istä', 'issä', 'tiin'],
+
+  // tuntureiden nimet
+  [['Suomu', 'Ruka', 'Tahko', 'Luosto', 'Malmi'], 0, 'lta', 'lla', 'lle'],
+  [['Levi', 'Pyhä'], 0, 'ltä', 'llä', 'lle'],
+
+  // totally weird cases & one-off hacks
+  [['Kangasala'], -3, 'alta', 'alla', 'alle'],
+
+  // Helsinki Lentoasema, Turenki Lentoasema (if such exists)
+  [['nki Lentoasema'], -14, 'ngin Lentoasemalta', 'ngin Lentoasemalla', 'ngin Lentoasemalle'],
+]
 
 function getLastMatch(city) {
-  return ruleset
-      .filter(rule => (
-        rule[0].filter((ending) => 
-          (city.toUpperCase().endsWith(ending.toUpperCase()))
-        ).length))
-      .map((match) => ({
-                         from: city.slice(0,city.length + match[1]) + match[2],
-                         in: city.slice(0,city.length + match[1]) + match[3],
-                         to: city.slice(0,city.length + match[1]) + match[4],
-                        }))
-      .pop() || {from: city, in: city, to: city};
+  return (
+    ruleset
+      .filter(
+        rule => rule[0].filter(ending => city.toUpperCase().endsWith(ending.toUpperCase())).length
+      )
+      .map(match => ({
+        from: city.slice(0, city.length + match[1]) + match[2],
+        in: city.slice(0, city.length + match[1]) + match[3],
+        to: city.slice(0, city.length + match[1]) + match[4],
+      }))
+      .pop() || { from: city, in: city, to: city }
+  )
 }
 
 module.exports = {
-    from: function(city) {
-        return getLastMatch(city).from;
-    },
-    in: function(city) {
-        return getLastMatch(city).in;
-    },
-    to: function(city) {
-        return getLastMatch(city).to;
-    },
-};
+  from: function(city) {
+    return getLastMatch(city).from
+  },
+  in: function(city) {
+    return getLastMatch(city).in
+  },
+  to: function(city) {
+    return getLastMatch(city).to
+  },
+}
--- test/index.js
+++ test/index.js
@@ -1,130 +1,134 @@
 var assert = require('assert'),
-    fromto = require('../index');
+  fromto = require('../index')
 
 const testdata = [
-    ['Tampere', 'Tampereelta', 'Tampereella', 'Tampereelle'],
-    ['Kauniainen', 'Kauniaisista', 'Kauniaisissa', 'Kauniaisiin'],
-    ['Asikainen', 'Asikaisista', 'Asikaisissa', 'Asikaisiin'],
-    ['Inkeroinen', 'Inkeroisista', 'Inkeroisissa', 'Inkeroisiin'],
-    ['Kauniainen', 'Kauniaisista', 'Kauniaisissa', 'Kauniaisiin'],
-    ['Jyväskylä', 'Jyväskylästä', 'Jyväskylässä', 'Jyväskylään'],
-    ['Asema', 'Asemalta', 'Asemalla', 'Asemalle'],
-    ['Leppävaara', 'Leppävaarasta', 'Leppävaarassa', 'Leppävaaraan'],
-    ['Maalaiskunta', 'Maalaiskunnasta', 'Maalaiskunnassa', 'Maalaiskuntaan'],
-    ['Mäntyharju', 'Mäntyharjulta', 'Mäntyharjulla', 'Mäntyharjulle'],
-    ['Hiekkaharju', 'Hiekkaharjusta', 'Hiekkaharjussa', 'Hiekkaharjuun'],
-    ['Ii', 'Iistä', 'Iissä', 'Iihin'],
-    ['Kera', 'Kerasta', 'Kerassa', 'Keraan'],
-    ['Kilo', 'Kilosta', 'Kilossa', 'Kiloon'],
-    ['Oulu', 'Oulusta', 'Oulussa', 'Ouluun'],
-    ['Degerby', 'Degerbystä', 'Degerbyssä', 'Degerbyhyn'],
-    ['Hanko', 'Hangosta', 'Hangossa', 'Hankoon'],
-    ['Kuhmo', 'Kuhmosta', 'Kuhmossa', 'Kuhmoon'],
-    ['Tammisaari', 'Tammisaaresta', 'Tammisaaressa', 'Tammisaareen'],
-    ['Seinäjoki', 'Seinäjoelta', 'Seinäjoella', 'Seinäjoelle'],
-    ['Kerimäki', 'Kerimäeltä', 'Kerimäellä', 'Kerimäelle'],
-    ['Retretti', 'Retretistä', 'Retretissä', 'Retrettiin'],
-    ['Taavetti', 'Taavetista', 'Taavetissa', 'Taavettiin'],
-    ['Naantali', 'Naantalista', 'Naantalissa', 'Naantaliin'],
-    ['Laukaa', 'Laukaasta', 'Laukaassa', 'Laukaaseen'],
-    ['Turku', 'Turusta', 'Turussa', 'Turkuun'],
-    ['Pertunmaa', 'Pertunmaalta', 'Pertunmaalla', 'Pertunmaalle'],
-    ['Espoo', 'Espoosta', 'Espoossa', 'Espooseen'],
-    ['Vantaa', 'Vantaalta', 'Vantaalla', 'Vantaalle'],
-    ['Kuopio', 'Kuopiosta', 'Kuopiossa', 'Kuopioon'],
-    ['Kouvola', 'Kouvolasta', 'Kouvolassa', 'Kouvolaan'],
-    ['Pori', 'Porista', 'Porissa', 'Poriin'],
-    ['Joensuu', 'Joensuusta', 'Joensuussa', 'Joensuuhun'],
-    ['Lappeenranta', 'Lappeenrannasta', 'Lappeenrannassa', 'Lappeenrantaan'],
-    ['Hämeenlinna', 'Hämeenlinnasta', 'Hämeenlinnassa', 'Hämeenlinnaan'],
-    ['Vaasa', 'Vaasasta', 'Vaasassa', 'Vaasaan'],
-    ['Jaala', 'Jaalasta', 'Jaalassa', 'Jaalaan'],
-    ['Laajavuori', 'Laajavuoresta', 'Laajavuoressa', 'Laajavuoreen'],
-    ['Palokka', 'Palokasta', 'Palokassa', 'Palokkaan'],
-    ['Ristonmaa', 'Ristonmaalta', 'Ristonmaalla', 'Ristonmaalle'],
-    ['Niipperi', 'Niipperistä', 'Niipperissä', 'Niipperiin'],
-    ['Lepsämä', 'Lepsämästä', 'Lepsämässä', 'Lepsämään'],
-    ['Toivakka', 'Toivakasta', 'Toivakassa', 'Toivakkaan'],
-    ['Lahti', 'Lahdesta', 'Lahdessa', 'Lahteen'],
-    ['Kitee', 'Kiteeltä', 'Kiteellä', 'Kiteelle'],
-    ['Joroinen', 'Joroisilta', 'Joroisilla', 'Joroisille'],
-    ['Vuontispirtti', 'Vuontispirtistä', 'Vuontispirtissä', 'Vuontispirttiin'],
-    ['Jorvas', 'Jorvaksesta', 'Jorvaksessa', 'Jorvakseen'],
-    ['Nurmes', 'Nurmeksesta', 'Nurmeksessa', 'Nurmekseen'],
-    ['Myllykoski', 'Myllykoskelta', 'Myllykoskella', 'Myllykoskelle'],
-    ['Tuuri', 'Tuurista', 'Tuurissa', 'Tuuriin'],
-    ['Hankasalmi', 'Hankasalmelta', 'Hankasalmella', 'Hankasalmelle'],
-    ['Kolari', 'Kolarista', 'Kolarissa', 'Kolariin'],
-    ['Lievestuore', 'Lievestuoreelta', 'Lievestuoreella', 'Lievestuoreelle'],
-    ['Hikiä', 'Hikiältä', 'Hikiällä', 'Hikiälle'],
-    ['Kirkkonummi', 'Kirkkonummelta', 'Kirkkonummella', 'Kirkkonummelle'],
-    ['Kilpisjärvi', 'Kilpisjärveltä', 'Kilpisjärvellä', 'Kilpisjärvelle'],
-    ['Koria', 'Korialta', 'Korialla', 'Korialle'],
-    ['Inari', 'Inarista', 'Inarissa', 'Inariin'],
-    ['Ruukki', 'Ruukista', 'Ruukissa', 'Ruukkiin'],
-    ['Rauma', 'Raumalta', 'Raumalla', 'Raumalle'],
-    ['Lapua', 'Lapualta', 'Lapualla', 'Lapualle'],
-    ['Kajaani', 'Kajaanista', 'Kajaanissa', 'Kajaaniin'],
-    ['Kannelmäki', 'Kannelmäestä', 'Kannelmäessä', 'Kannelmäkeen'],
-    ['Kangasala', 'Kangasalta', 'Kangasalla', 'Kangasalle'],
-    ['Punavuori', 'Punavuoresta', 'Punavuoressa', 'Punavuoreen'],
-    ['Imatra', 'Imatralta', 'Imatralla', 'Imatralle'],
-    ['Sumatra', 'Sumatralta', 'Sumatralla', 'Sumatralle'],
-    ['Raahe', 'Raahesta', 'Raahessa', 'Raaheen'],
-    ['Uimaharju', 'Uimaharjusta', 'Uimaharjussa', 'Uimaharjuun'], // src: http://www.uimaharju.fi/
-    ['Hyvinkää', 'Hyvinkäältä', 'Hyvinkäällä', 'Hyvinkäälle'],
-    ['Luosto', 'Luostolta', 'Luostolla', 'Luostolle'], // luosto.fi
-    ['XY Motelli', 'XY Motellista', 'XY Motellissa', 'XY Motelliin'], 
-    ['Raahe', 'Raahesta', 'Raahessa', 'Raaheen'], // luosto.fi
-    ['Ruka', 'Rukalta', 'Rukalla', 'Rukalle'], 
-    ['Pyhä', 'Pyhältä', 'Pyhällä', 'Pyhälle'], 
-    ['Kiuruvesi', 'Kiuruvedeltä', 'Kiuruvedellä', 'Kiuruvedelle'], 
-    ['Karjaa', 'Karjaalta', 'Karjaalla', 'Karjaalle'], // www.raasepori.fi 
-    ['Pukinmäki', 'Pukinmäestä', 'Pukinmäessä', 'Pukinmäkeen'], // www.raasepori.fi 
-    ['Dragsvik', 'Dragsvikistä', 'Dragsvikissä', 'Dragsvikiin'], // https://fi.wikipedia.org/wiki/Uudenmaan_prikaati 
-    ['Runni', 'Runnista', 'Runnissa', 'Runniin'], 
-    ['Keuruu', 'Keuruulta', 'Keuruulla', 'Keuruulle'],
-    ['Pihlajavesi', 'Pihlajavedeltä', 'Pihlajavedellä', 'Pihlajavedelle'],
-    ['Saariselkä', 'Saariselältä', 'Saariselällä', 'Saariselälle'],
-    ['Harjavalta', 'Harjavallasta', 'Harjavallassa', 'Harjavaltaan'],
-    ['Kiilopää', 'Kiilopäältä', 'Kiilopäällä', 'Kiilopäälle'],
-    ['Tahko', 'Tahkolta', 'Tahkolla', 'Tahkolle'],
-    ['Suomu', 'Suomulta', 'Suomulla', 'Suomulle'],
-    ['Malmi', 'Malmilta', 'Malmilla', 'Malmille'],
-    ['Lapinlahti', 'Lapinlahdelta', 'Lapinlahdella', 'Lapinlahdelle'],
-    ['Kauhava', 'Kauhavalta', 'Kauhavalla', 'Kauhavalle'],
-    ['Kerava', 'Keravalta', 'Keravalla', 'Keravalle'],
-    ['Orivesi', 'Orivedeltä', 'Orivedellä', 'Orivedelle'],
-    ['Muhos', 'Muhokselta', 'Muhoksella', 'Muhokselle'],
-    ['Kannus', 'Kannuksesta', 'Kannuksessa', 'Kannukseen'],
-    ['Varkaus', 'Varkaudesta', 'Varkaudessa', 'Varkauteen'],
-    ['Koivuhovi', 'Koivuhovista', 'Koivuhovissa', 'Koivuhoviin'],
-    ['Nokia', 'Nokialta', 'Nokialla', 'Nokialle'],
-    ['Vihanti', 'Vihantista', 'Vihantissa', 'Vihantiin'],
-    ['Järvenpää', 'Järvenpäästä', 'Järvenpäässä', 'Järvenpäähän'],
-    ['Savio', 'Saviolta', 'Saviolla', 'Saviolle'],
-    ['Myyrmäki', 'Myyrmäestä', 'Myyrmäessä', 'Myyrmäkeen'],
-    //this is ugly, but Helsinki Lentoasema is really called Helsinki Lentoasema
-    ['Helsinki Lentoasema', 'Helsingin Lentoasemalta', 'Helsingin Lentoasemalla', 'Helsingin Lentoasemalle'],
-    
-    ['XY-Tunturi', 'XY-Tunturilta', 'XY-Tunturilla', 'XY-Tunturille'],
-    ['XY Motelli', 'XY Motellista', 'XY Motellissa', 'XY Motelliin'], 
-    ['XY-Tunturi', 'XY-Tunturilta', 'XY-Tunturilla', 'XY-Tunturille'],
+  ['Tampere', 'Tampereelta', 'Tampereella', 'Tampereelle'],
+  ['Kauniainen', 'Kauniaisista', 'Kauniaisissa', 'Kauniaisiin'],
+  ['Asikainen', 'Asikaisista', 'Asikaisissa', 'Asikaisiin'],
+  ['Inkeroinen', 'Inkeroisista', 'Inkeroisissa', 'Inkeroisiin'],
+  ['Kauniainen', 'Kauniaisista', 'Kauniaisissa', 'Kauniaisiin'],
+  ['Jyväskylä', 'Jyväskylästä', 'Jyväskylässä', 'Jyväskylään'],
+  ['Asema', 'Asemalta', 'Asemalla', 'Asemalle'],
+  ['Leppävaara', 'Leppävaarasta', 'Leppävaarassa', 'Leppävaaraan'],
+  ['Maalaiskunta', 'Maalaiskunnasta', 'Maalaiskunnassa', 'Maalaiskuntaan'],
+  ['Mäntyharju', 'Mäntyharjulta', 'Mäntyharjulla', 'Mäntyharjulle'],
+  ['Hiekkaharju', 'Hiekkaharjusta', 'Hiekkaharjussa', 'Hiekkaharjuun'],
+  ['Ii', 'Iistä', 'Iissä', 'Iihin'],
+  ['Kera', 'Kerasta', 'Kerassa', 'Keraan'],
+  ['Kilo', 'Kilosta', 'Kilossa', 'Kiloon'],
+  ['Oulu', 'Oulusta', 'Oulussa', 'Ouluun'],
+  ['Degerby', 'Degerbystä', 'Degerbyssä', 'Degerbyhyn'],
+  ['Hanko', 'Hangosta', 'Hangossa', 'Hankoon'],
+  ['Kuhmo', 'Kuhmosta', 'Kuhmossa', 'Kuhmoon'],
+  ['Tammisaari', 'Tammisaaresta', 'Tammisaaressa', 'Tammisaareen'],
+  ['Seinäjoki', 'Seinäjoelta', 'Seinäjoella', 'Seinäjoelle'],
+  ['Kerimäki', 'Kerimäeltä', 'Kerimäellä', 'Kerimäelle'],
+  ['Retretti', 'Retretistä', 'Retretissä', 'Retrettiin'],
+  ['Taavetti', 'Taavetista', 'Taavetissa', 'Taavettiin'],
+  ['Naantali', 'Naantalista', 'Naantalissa', 'Naantaliin'],
+  ['Laukaa', 'Laukaasta', 'Laukaassa', 'Laukaaseen'],
+  ['Turku', 'Turusta', 'Turussa', 'Turkuun'],
+  ['Pertunmaa', 'Pertunmaalta', 'Pertunmaalla', 'Pertunmaalle'],
+  ['Espoo', 'Espoosta', 'Espoossa', 'Espooseen'],
+  ['Vantaa', 'Vantaalta', 'Vantaalla', 'Vantaalle'],
+  ['Kuopio', 'Kuopiosta', 'Kuopiossa', 'Kuopioon'],
+  ['Kouvola', 'Kouvolasta', 'Kouvolassa', 'Kouvolaan'],
+  ['Pori', 'Porista', 'Porissa', 'Poriin'],
+  ['Joensuu', 'Joensuusta', 'Joensuussa', 'Joensuuhun'],
+  ['Lappeenranta', 'Lappeenrannasta', 'Lappeenrannassa', 'Lappeenrantaan'],
+  ['Hämeenlinna', 'Hämeenlinnasta', 'Hämeenlinnassa', 'Hämeenlinnaan'],
+  ['Vaasa', 'Vaasasta', 'Vaasassa', 'Vaasaan'],
+  ['Jaala', 'Jaalasta', 'Jaalassa', 'Jaalaan'],
+  ['Laajavuori', 'Laajavuoresta', 'Laajavuoressa', 'Laajavuoreen'],
+  ['Palokka', 'Palokasta', 'Palokassa', 'Palokkaan'],
+  ['Ristonmaa', 'Ristonmaalta', 'Ristonmaalla', 'Ristonmaalle'],
+  ['Niipperi', 'Niipperistä', 'Niipperissä', 'Niipperiin'],
+  ['Lepsämä', 'Lepsämästä', 'Lepsämässä', 'Lepsämään'],
+  ['Toivakka', 'Toivakasta', 'Toivakassa', 'Toivakkaan'],
+  ['Lahti', 'Lahdesta', 'Lahdessa', 'Lahteen'],
+  ['Kitee', 'Kiteeltä', 'Kiteellä', 'Kiteelle'],
+  ['Joroinen', 'Joroisilta', 'Joroisilla', 'Joroisille'],
+  ['Vuontispirtti', 'Vuontispirtistä', 'Vuontispirtissä', 'Vuontispirttiin'],
+  ['Jorvas', 'Jorvaksesta', 'Jorvaksessa', 'Jorvakseen'],
+  ['Nurmes', 'Nurmeksesta', 'Nurmeksessa', 'Nurmekseen'],
+  ['Myllykoski', 'Myllykoskelta', 'Myllykoskella', 'Myllykoskelle'],
+  ['Tuuri', 'Tuurista', 'Tuurissa', 'Tuuriin'],
+  ['Hankasalmi', 'Hankasalmelta', 'Hankasalmella', 'Hankasalmelle'],
+  ['Kolari', 'Kolarista', 'Kolarissa', 'Kolariin'],
+  ['Lievestuore', 'Lievestuoreelta', 'Lievestuoreella', 'Lievestuoreelle'],
+  ['Hikiä', 'Hikiältä', 'Hikiällä', 'Hikiälle'],
+  ['Kirkkonummi', 'Kirkkonummelta', 'Kirkkonummella', 'Kirkkonummelle'],
+  ['Kilpisjärvi', 'Kilpisjärveltä', 'Kilpisjärvellä', 'Kilpisjärvelle'],
+  ['Koria', 'Korialta', 'Korialla', 'Korialle'],
+  ['Inari', 'Inarista', 'Inarissa', 'Inariin'],
+  ['Ruukki', 'Ruukista', 'Ruukissa', 'Ruukkiin'],
+  ['Rauma', 'Raumalta', 'Raumalla', 'Raumalle'],
+  ['Lapua', 'Lapualta', 'Lapualla', 'Lapualle'],
+  ['Kajaani', 'Kajaanista', 'Kajaanissa', 'Kajaaniin'],
+  ['Kannelmäki', 'Kannelmäestä', 'Kannelmäessä', 'Kannelmäkeen'],
+  ['Kangasala', 'Kangasalta', 'Kangasalla', 'Kangasalle'],
+  ['Punavuori', 'Punavuoresta', 'Punavuoressa', 'Punavuoreen'],
+  ['Imatra', 'Imatralta', 'Imatralla', 'Imatralle'],
+  ['Sumatra', 'Sumatralta', 'Sumatralla', 'Sumatralle'],
+  ['Raahe', 'Raahesta', 'Raahessa', 'Raaheen'],
+  ['Uimaharju', 'Uimaharjusta', 'Uimaharjussa', 'Uimaharjuun'], // src: http://www.uimaharju.fi/
+  ['Hyvinkää', 'Hyvinkäältä', 'Hyvinkäällä', 'Hyvinkäälle'],
+  ['Luosto', 'Luostolta', 'Luostolla', 'Luostolle'], // luosto.fi
+  ['XY Motelli', 'XY Motellista', 'XY Motellissa', 'XY Motelliin'],
+  ['Raahe', 'Raahesta', 'Raahessa', 'Raaheen'], // luosto.fi
+  ['Ruka', 'Rukalta', 'Rukalla', 'Rukalle'],
+  ['Pyhä', 'Pyhältä', 'Pyhällä', 'Pyhälle'],
+  ['Kiuruvesi', 'Kiuruvedeltä', 'Kiuruvedellä', 'Kiuruvedelle'],
+  ['Karjaa', 'Karjaalta', 'Karjaalla', 'Karjaalle'], // www.raasepori.fi
+  ['Pukinmäki', 'Pukinmäestä', 'Pukinmäessä', 'Pukinmäkeen'], // www.raasepori.fi
+  ['Dragsvik', 'Dragsvikistä', 'Dragsvikissä', 'Dragsvikiin'], // https://fi.wikipedia.org/wiki/Uudenmaan_prikaati
+  ['Runni', 'Runnista', 'Runnissa', 'Runniin'],
+  ['Keuruu', 'Keuruulta', 'Keuruulla', 'Keuruulle'],
+  ['Pihlajavesi', 'Pihlajavedeltä', 'Pihlajavedellä', 'Pihlajavedelle'],
+  ['Saariselkä', 'Saariselältä', 'Saariselällä', 'Saariselälle'],
+  ['Harjavalta', 'Harjavallasta', 'Harjavallassa', 'Harjavaltaan'],
+  ['Kiilopää', 'Kiilopäältä', 'Kiilopäällä', 'Kiilopäälle'],
+  ['Tahko', 'Tahkolta', 'Tahkolla', 'Tahkolle'],
+  ['Suomu', 'Suomulta', 'Suomulla', 'Suomulle'],
+  ['Malmi', 'Malmilta', 'Malmilla', 'Malmille'],
+  ['Lapinlahti', 'Lapinlahdelta', 'Lapinlahdella', 'Lapinlahdelle'],
+  ['Kauhava', 'Kauhavalta', 'Kauhavalla', 'Kauhavalle'],
+  ['Kerava', 'Keravalta', 'Keravalla', 'Keravalle'],
+  ['Orivesi', 'Orivedeltä', 'Orivedellä', 'Orivedelle'],
+  ['Muhos', 'Muhokselta', 'Muhoksella', 'Muhokselle'],
+  ['Kannus', 'Kannuksesta', 'Kannuksessa', 'Kannukseen'],
+  ['Varkaus', 'Varkaudesta', 'Varkaudessa', 'Varkauteen'],
+  ['Koivuhovi', 'Koivuhovista', 'Koivuhovissa', 'Koivuhoviin'],
+  ['Nokia', 'Nokialta', 'Nokialla', 'Nokialle'],
+  ['Vihanti', 'Vihantista', 'Vihantissa', 'Vihantiin'],
+  ['Järvenpää', 'Järvenpäästä', 'Järvenpäässä', 'Järvenpäähän'],
+  ['Savio', 'Saviolta', 'Saviolla', 'Saviolle'],
+  ['Myyrmäki', 'Myyrmäestä', 'Myyrmäessä', 'Myyrmäkeen'],
+  //this is ugly, but Helsinki Lentoasema is really called Helsinki Lentoasema
+  [
+    'Helsinki Lentoasema',
+    'Helsingin Lentoasemalta',
+    'Helsingin Lentoasemalla',
+    'Helsingin Lentoasemalle',
+  ],
 
-    ['XYZ', 'XYZ', 'XYZ', 'XYZ'],
-];
+  ['XY-Tunturi', 'XY-Tunturilta', 'XY-Tunturilla', 'XY-Tunturille'],
+  ['XY Motelli', 'XY Motellista', 'XY Motellissa', 'XY Motelliin'],
+  ['XY-Tunturi', 'XY-Tunturilta', 'XY-Tunturilla', 'XY-Tunturille'],
 
+  ['XYZ', 'XYZ', 'XYZ', 'XYZ'],
+]
 
-testdata.map(function (item) {
-    describe(`*** ${item[0]} ***`, function() {
-        it(`From should be ${item[1]}`, function() {
-            assert.equal(fromto.from(item[0]), item[1]);
-        });
-        it(`In should be ${item[2]}`, function() {
-            assert.equal(fromto.in(item[0]), item[2]);
-        });
-        it(`To should be ${item[3]}`, function() {
-            assert.equal(fromto.to(item[0]), item[3]);
-        });
-    });
-});
+testdata.map(function(item) {
+  describe(`*** ${item[0]} ***`, function() {
+    it(`From should be ${item[1]}`, function() {
+      assert.equal(fromto.from(item[0]), item[1])
+    })
+    it(`In should be ${item[2]}`, function() {
+      assert.equal(fromto.in(item[0]), item[2])
+    })
+    it(`To should be ${item[3]}`, function() {
+      assert.equal(fromto.to(item[0]), item[3])
+    })
+  })
+})
